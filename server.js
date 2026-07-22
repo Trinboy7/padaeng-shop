dd9b8b98061c061795c7ff69dbe71f66ac52f9e7
Trinboy7 <trinboy7@gmail.com>
2026-07-22 15:18:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
+    // รับออเดอร์ใหม่จากลูกค้า
     socket.on('new-order', (data) => io.emit('update-kitchen', data));
+
+    // รับคำสั่งยกเลิกออเดอร์จากลูกค้า และส่งบอกหน้าจอครัวทันที
+    socket.on('cancel-order', (data) => io.emit('cancel-kitchen', data));
 });
 
 http.listen(3000, () => console.log('ร้านป้าแดงเปิดแล้วที่ http://localhost:3000'));
